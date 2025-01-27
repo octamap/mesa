fba3952eee824aac44e74aee7096439d8fcfec75
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2025-01-27 12:44:28 +0100

    - Better logs

--- src/log.ts
+++ src/log.ts
@@ -3,5 +3,14 @@ import logText from './logText.js';
 export default function log(text: string, type?: "warn" | "error") {
     const methods = { "warn": console.warn, "error": console.error }
     const method = type ? methods[type] : console.log
-    method(logText(text))
+    const suffix = (() => {
+        switch (type) {
+            case "error":
+                return " ❌ -"
+            case "warn":
+                return " ❓ -"
+        }
+        return ""
+    })()
+    method(logText(`${suffix}${text}`))
 }
